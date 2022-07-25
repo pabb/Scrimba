55437edcd3f67b67189cbfc45afe6e69f03839f3
Pabb <1515359+pabb@users.noreply.github.com>
2022-07-25 12:17:42 -0400

    Fix question/answer data structures and get buttons displaying with IDs

--- learnreact/quizzical_app/src/components/Quiz.jsx
+++ learnreact/quizzical_app/src/components/Quiz.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { nanoid } from "nanoid";
 import { decode } from "he";
-import types from '../types.jsx';
 
 // TODO: if question count is > 5, paginate and generate a "Next page" button
 //          but then figure out how to do "checkAnswers" (because they won't all be on one page)
 //          maybe just generate a new component to show the user which ones they missed
 export default function Quiz(props) {
     
-    const [questions, setQuestions] = React.useState([]);
-    const [answer, setAnswer] = React.useState(0); // TODO: set this to the index of the button with the answer
-
+    const [questions, setQuestions] = React.useState([
+        {
+            id: "gjgjgjgjg",
+            correctAnswer: "Yes",
+            correctAnswerId: "abcde",
+            incorrectAnswers: ["No", "Maybe"],
+            question: "Is this a test?",
+            isBoolean: false
+        }
+    ]);
     // Query API for questions on first component load
     React.useEffect(() => {
         async function getQuestions() {
@@ -20,29 +26,49 @@ export default function Quiz(props) {
                 throw "ERROR: Unable to fetch questions from OTDB API";
             }
 
-            console.log(data.results);
-            setQuestions(data.results);
+            const rawQuestions = data.results;
+            assignQuestions(rawQuestions);
         }
         getQuestions();
     }, [])
 
-    function generateAnswers(question) {
-        let answers = question.incorrect_answers.slice();
-        answers.push(question.correct_answer);
-        answers.sort();
-
-        // Save correct answer index
-        let answerElems = []
-        if (question.type === types.QUESTION_TYPE_MC) {
-            answerElems = answers.map(answer => {
-                return <button key={nanoid()} className="answer--button">{answer}</button>
+    function assignQuestions(rawQuestions) {
+        const modifiedQuestions = []
+        for (let i = 0; i < rawQuestions.length; i++) {
+            const currentQuestion = rawQuestions[i]
+            modifiedQuestions.push({
+                id: nanoid(),
+                correctAnswer: currentQuestion.correct_answer,
+                correctAnswerId: nanoid(),
+                incorrectAnswers: currentQuestion.incorrect_answers,
+                question: currentQuestion.question,
+                isBoolean: currentQuestion.type === "boolean"
             })
-        } else if (question.type === types.QUESTION_TYPE_TF) {
-            answerElems.push(<button key={nanoid()} className="answer--button">{types.ANSWER_TRUE}</button>)
-            answerElems.push(<button key={nanoid()} className="answer--button">{types.ANSWER_FALSE}</button>)
-        } else {
-            throw "ERROR: Invalid question type provided";
         }
+
+        setQuestions(modifiedQuestions);
+    }
+
+    function generateAnswers(question) {
+        let answers = question.incorrectAnswers.slice();
+        answers.unshift(question.correctAnswer); // Put correct answer at front of array so that we can assign its id as correctAnswerId below
+
+        const answerElems = answers.map((answer, index) => {
+            const answerId = index === 0 ? question.correctAnswerId : nanoid();
+            return <button key={answerId} id={answerId} className="answer--button">{answer}</button>
+        })
+
+        // Shuffle ordering of multiple choice elements so the answer isn't always the first choice
+        if (!question.isBoolean) {
+            // shuffle(answerElems);
+            answerElems.sort(function() {
+                return Math.random() - 0.5;
+            })
+        }
+        // Consider setting buttons for true/false questions in static order (True, False) 
+        // else {
+        //     answerElems.sort(button => button.value)
+        // }
 
         return (
             <div className="answers">
@@ -53,7 +79,7 @@ export default function Quiz(props) {
 
     function generateQuestions(count) {
         const questionElems = questions.map(question => (
-            <div className="question" key={nanoid()}>
+            <div className="question" key={question.id}>
                 <h3 className="question--text">{decode(question.question)}</h3>
                 {generateAnswers(question)}
                 <br/>
