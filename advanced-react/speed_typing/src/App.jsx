7fba49e8e19397152a70105df821a701ce6fe76a
Pabb <1515359+pabb@users.noreply.github.com>
2022-08-16 13:39:28 -0400

    Move all business logic to a custom hook

--- advanced-react/speed_typing/src/App.jsx
+++ advanced-react/speed_typing/src/App.jsx
@@ -1,46 +1,17 @@
-import {useState, useEffect, useRef} from "react"
-import './App.css'
+import React from "react"
+import useWordGame from "./hooks/useWordGame"
+import "./App.css"
 
 function App() {
-    const STARTING_TIME = 5
-    
-    const [text, setText] = useState("")
-    const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = useState(false)
-    const [wordCount, setWordCount] = useState(0)
-    const textBoxRef = useRef(null)
-    
-    function handleChange(e) {
-        const {value} = e.target
-        setText(value)
-    }
-    
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-    
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-    }
-    
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-    
-    useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            textBoxRef.current.focus()
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning])
+    const {
+        textBoxRef, 
+        handleChange, 
+        text, 
+        isTimeRunning, 
+        timeRemaining, 
+        startGame, 
+        wordCount
+    } = useWordGame(5)
     
     return (
         <div>
