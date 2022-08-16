b420bee282c39b2ce1808200aa670f0011eea100
Pabb <1515359+pabb@users.noreply.github.com>
2022-08-16 13:16:18 -0400

    Add toggler mini-project to demo custom hooks

---
+++ advanced-react/custom_hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import Menu from "./Menu"
+import Favorite from "./Favorite"
+import './App.css'
+
+function App() {
+  return (
+      <div>
+          <Menu />
+          <hr />
+          <Favorite />
+      </div>
+  )
+}
+
+export default App
