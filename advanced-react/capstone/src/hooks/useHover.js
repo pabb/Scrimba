54c3e775b4eaac3f79bc7406f16eeb9284ab015b
Pabb <1515359+pabb@users.noreply.github.com>
2022-09-03 16:22:43 -0400

    Add Advanced React's capstone project Convert React router v5 -> v6 Add prettier/eslint Add conditional checks on useHover hook Note to self: save all the files before attempting to build next time

---
+++ advanced-react/capstone/src/hooks/useHover.js
@@ -0,0 +1,32 @@
+import { useState, useEffect, useRef } from 'react';
+
+function useHover() {
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef(null);
+
+  function enter() {
+    setHovered(true);
+  }
+
+  function leave() {
+    setHovered(false);
+  }
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener('mouseenter', enter);
+      ref.current.addEventListener('mouseleave', leave);
+    }
+
+    return () => {
+      if (ref.current) {
+        ref.current.removeEventListener('mouseenter', enter);
+        ref.current.removeEventListener('mouseleave', leave);
+      }
+    };
+  }, []);
+
+  return [hovered, ref];
+}
+
+export default useHover;
